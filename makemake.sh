0553f349d659f8ee4e95efce4a1d5c4cd084ec85
sdelator <sdelatorre@ucdavis.edu>
2015-01-28 22:04:17 -0800

    Update makemake.sh

--- makemake.sh
+++ makemake.sh
@@ -1,37 +1,63 @@
 #! /bin/bash
 if [[ "$#" -eq 0 ]]
-then
+  then
   echo Executable name required.
   echo usage: makemake.sh executable_name
-else
+else # initialize Makefile
+
+  # Line 1
   echo -n "$1 :">Makefile
-fi
-for f in *.cpp ; do  #make .cpp
-  f2=${f%.*}.o
-  echo -n " $f2">>Makefile
-done # for
-  echo>>makefile
-  echo -e '\n' '\t' "g++ -ansi -Wall -g -o"'\c'>>Makefile 
-  for f in *.cpp ; do
-    f2=${f%.*}.o
-    echo -n " $f2" >>Makefile
+  for f in *.cpp ; do # finds all .cpp files
+    f2=${f%.*}.o # changes them to .o strings
+    echo -n " $f2">>Makefile # appends the new string to the Makefile
   done # for
-  echo>>Makefile
-  #completed first 
-   
-  for f in *.cpp ; do
+  echo -n '  '>>Makefile
+  echo>>Makefile # adds a new line
+  
+  # Line 2
+  echo -e -n '\tg++ -ansi -Wall -g -o '"$1">>Makefile
+  for f in *.cpp ; do # for every .cpp file
     f2=${f%.*}.o
-    echo -n " $f2 :" >>Makefile
-    awk '/"*.h"/ {print $2}' $f #replace $f2 with regular file
-    echo $f #DELETE DELETE 
-    echo
-        
-  done #for
-
-
-
-
-
+    echo -n " $f2">>Makefile # appends the new string to the Makefile
+  done # for
+  echo -n '  '>>Makefile
+  echo>>Makefile
+  echo>>Makefile
+  
+  # Line 3+
+  for f in *.cpp ; do # for every .cpp file
+    f2=${f%.*}.o
+    echo -n "$f2 : ">>Makefile
+    echo -n "$f ">>Makefile
+    awk '/"*.h"/ {print $2}' $f > out.txt # search for .h
+    # cat out.txt
+    sed 's/"//g' out.txt > out1.txt # remove the "" in the line
+    # cat out1.txt
+    # echo yay
+    tr '\n' ' ' < out1.txt > out2.txt
+    # cat out2.txt
+    cat out2.txt>>Makefile
+    echo>>Makefile
+    
+  # Line 4+
+    echo -e -n '\tg++ -ansi -Wall -g -c' "$f">>Makefile
+    echo>>Makefile
+    echo>>Makefile
+  done
+  rm out.txt
+  rm out1.txt
+  rm out2.txt
+  
+  # Last Line
+  echo 'clean : '>>Makefile
+  echo -n -e '\trm -f'" $1">>Makefile 
+  for f in *.cpp ; do # for every .cpp file
+    f2=${f%.*}.o
+    echo -n " $f2 ">>Makefile # appends the new string to the Makefile
+  done # for
+  echo -n ' '>>Makefile
+  echo ' '>>Makefile
+fi
 # Helpful Hints
 # Echo Tabs: echo -e ' \t'
 # -e: enable interpret of backslash
